6bc92bd54911bc98a4de7515f5e5dba8c837e638
Marat Sattarov <sattarovmk@yandex.ru>
2023-06-17 00:32:07 +0500

    Chore (root) configuration and setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    base: '/2d-planner/',
+    server: {
+        host: true,
+        port: 3000,
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@import "@/styles/vars.scss";`,
+            },
+        },
+    },
+    resolve: {
+        alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    },
+});
